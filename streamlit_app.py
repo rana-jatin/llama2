7a7de2aa4c91d00e2083423e502bd4550df2f5ef
rana-jatin <163754674+rana-jatin@users.noreply.github.com>
2025-02-01 13:43:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.sidebar:
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=20, max_value=80, value=50, step=5)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
